d53a3bbf19e28bf462272e0a51288137899bcb1c
Mikk Noorkõiv <mikk.noorkoiv@me.com>
2018-04-09 21:04:51 +0300

    added comments in viewcontroller.swift

--- Traffic Light Segues/ViewController.swift
+++ Traffic Light Segues/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var segueSwitch: UISwitch!
+    @IBOutlet weak var segueSwitch: UISwitch! // Switch to allow or disallow button to call segue
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func pushToYellow(_ sender: Any) {
-        if segueSwitch.isOn {
-            performSegue(withIdentifier: "Yellow", sender: nil)
+    @IBAction func pushToYellow(_ sender: Any) { // Action is called from interface builder segue
+        if segueSwitch.isOn {                    // Check if switch is on or off
+            performSegue(withIdentifier: "Yellow", sender: nil) // "Yellow" button is connected to yellow viewcontroller in IB
+            // Segue's identifier must be set in interface builder
         }
     }
     
