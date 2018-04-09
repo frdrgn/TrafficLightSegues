1c2dc6ab90bcf0942279900a1d73af125c41018d
Mikk Noorkõiv <mikk.noorkoiv@me.com>
2018-04-09 17:55:39 +0300

    first commit

--- Traffic Light Segues/ViewController.swift
+++ Traffic Light Segues/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pushToYellow(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
     }
-
-
+    
+    @IBAction func pushToGreen(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
 }
 
